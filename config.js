002b887ae4ff7694e4537531892158c926ee7543
Francisco de Oliveira Neto <francisco@Mac-mini-de-Francisco.local>
2026-04-19 18:23:31 +0100

    Add config.js with OAuth client IDs

---
+++ config.js
@@ -0,0 +1,16 @@
+export const CONFIG = {
+  youtube: {
+    clientId: "262962134043-mfsu26colfpnt1h7sa5uo069qos19r4g.apps.googleusercontent.com",
+    scopes: ["https://www.googleapis.com/auth/youtube.readonly"],
+  },
+
+  spotify: {
+    clientId: "6368f4e037a04653aa3dd32bc2132b11",
+    scopes: ["user-follow-read", "user-library-read", "user-read-playback-position"],
+  },
+
+  daysToShow: 30,
+
+  gistFilename: "yt-spotify-feed-state.json",
+  gistDescription: "yt-spotify-feed state (private)",
+};
